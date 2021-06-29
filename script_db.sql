f847d58abc28209148123b9a19093f4a9f67cd1c
dammich <damien.michaudel@gmail.com>
2021-06-29 23:39:19 +0200

    UPDATE : SQL script

--- script_db.sql
+++ script_db.sql
@@ -3,6 +3,8 @@
 INSERT INTO "Roles" ("type", "createdAt", "updatedAt") VALUES ('Admin', now(), now());
 -- Role pour un utilisateur qui ne peut que consulter le site (si on en créer un plus tard)
 INSERT INTO "Roles" ("type", "createdAt", "updatedAt") VALUES ('Viewer', now(), now());
+-- Role pour un Bot
+INSERT INTO "Roles" ("type", "createdAt", "updatedAt") VALUES ('Bot', now(), now());
 
 
 /* AJOUT DES UTILISATEURS */
@@ -16,3 +18,6 @@ VALUES ('dany', '01250041e7879922ed151be3e152b2e4725e2d1e7fbe2e61d19dadea48a49cf
 
 INSERT INTO "Users" ("username", "password", "lastLogin", "roleId", "createdAt", "updatedAt")
 VALUES ('sam', '01250041e7879922ed151be3e152b2e4725e2d1e7fbe2e61d19dadea48a49cf1', now(), (SELECT id FROM "Roles" WHERE type='Admin'), now(), now());
+
+INSERT INTO "Users" ("username", "password", "lastLogin", "roleId", "createdAt", "updatedAt")
+VALUES ('bot', '138223db4efd5a88b209da45bd55b4fe4822e4e5b3b691bb33f7a07be6420c0b', now(), (SELECT id FROM "Roles" WHERE type='Bot'), now(), now());
