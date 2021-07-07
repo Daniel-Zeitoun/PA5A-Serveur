cda3d015ac1d34822db6a252f2525bf696044864
dammich <damien.michaudel@gmail.com>
2021-07-07 17:11:09 +0200

    UPDATE : Send reverse-shell command at reload

--- application/src/static/js/script.js
+++ application/src/static/js/script.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
 
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
-    });
+    })
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar, #content').toggleClass('active')
         $('.collapse.in').toggleClass('in')
         $('a[aria-expanded=true]').attr('aria-expanded', 'false')
-    });
+    })
 
-});
+})
 
 /* ---------------------------------------------------
     VICTIMS PAGE : Open link to access specific victim
@@ -21,8 +21,8 @@ $(document).ready(function () {
 $(document).ready(function ($) {
     $('*[data-href]').not($('.btn-command')).on('click', function () {
         window.location = $(this).data("href")
-    });
-});
+    })
+})
 
 
 /* ---------------------------------------------------
@@ -32,30 +32,33 @@ $(document).ready(function ($) {
 
     $('.btn-command').on('click', function () {
 
-        const command = JSON.stringify({ "name": $(this).data("command").toUpperCase() })
-        const url = new URL(window.location.origin + '/api/clients/' + $(this).data("uuid") + '/commands')
-
-        $.ajax({
-            type: "POST",
-            data: command,
-            url: url,
-            contentType: "application/json"
-        })
-            .done((data) => {
-                console.log("The AJAX request is a success !")
-                console.log(data)
-                $('#commandSent').show("slow")
-                //alert("Command successfully added to the queue !")
-            })
-            .fail(() => {
-                console.log("The AJAX request failed ...")
-                alert("An error occurred ...")
-            })
-
         // If it's a shell's request, then we open the shell interface in a new tab
         if ($(this).data("command") === "reverse-shell") {
             const url = new URL(window.location.origin + '/app/command/shell/' + $(this).data("uuid"))
             window.open(url, '_blank')
+            $('#commandSent').fadeIn(1000)
+            $('#commandSent').delay(3000).fadeOut(1000)
         }
-    });
-});
+        // Else, we just send the command
+        else {
+            const command = JSON.stringify({ "name": $(this).data("command").toUpperCase() })
+            const url = new URL(window.location.origin + '/api/clients/' + $(this).data("uuid") + '/commands')
+    
+            $.ajax({
+                type: "POST",
+                data: command,
+                url: url,
+                contentType: "application/json"
+            })
+                .done((data) => {
+                    console.log("The AJAX request to send command is a success !")
+                    $('#commandSent').fadeIn(1000)
+                    $('#commandSent').delay(3000).fadeOut(1000)
+                })
+                .fail(() => {
+                    console.log("The AJAX request to send command failed ...")
+                    alert("An error occurred ...")
+                })
+        }
+    })
+})
